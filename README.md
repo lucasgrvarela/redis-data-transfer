094956ee832fc9f336cf7c1b399596903644f839
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2023-07-25 13:08:55 -0300

    improve code speed and readme.md

--- README.md
+++ README.md
@@ -10,7 +10,21 @@ go build
 
 2. Run it
 ```
+$ redis-cli keys "*"
+(empty array)
+
 $ ./redis-data-transfer -old redis-url-123.abc:6379 -new localhost:6379
-2023/07/24 10:49:29 Starting the Redis data transfer application...
-   8% |█████                                                                | (875/10000, 2 it/s) [8m42s:1h36m15s]
+2023/07/25 13:04:25 Starting the Redis data transfer application...
+ 100% |███████████████████████████████████████████████| (10000/10000, 469 it/s)         
+2023/07/25 13:04:50 Data transfer completed successfully.
+2023/07/25 13:04:50 Number of keys in the old Redis database: 10000
+2023/07/25 13:04:50 Number of keys in the new Redis database: 10000
+
+$ redis-cli keys "*"
+    1) "key2657"
+    2) "key8365"
+    3) "key6011"
+    4) "key4759"
+    5) "key9559"
+        ...
 ```
--- main.go
+++ main.go
@@ -7,30 +7,25 @@ import (
 	"log"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
-	// Define flags to receive Redis connection strings from the command-line arguments
 	var oldRedisURL, newRedisURL string
 	flag.StringVar(&oldRedisURL, "old", "", "Old Redis connection string (e.g., '[user]:[pass]@url:port')")
 	flag.StringVar(&newRedisURL, "new", "", "New Redis connection string (e.g., '[user]:[pass]@url:port')")
 	flag.Parse()
 
-	// Ensure both old and new Redis connection strings are provided
 	if oldRedisURL == "" || newRedisURL == "" {
 		fmt.Println("Please provide the old and new Redis connection strings using the -old and -new flags.")
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	// Initialize the logger
 	log.Println("Starting the Redis data transfer application...")
 
-	// Create Redis clients for the old and new Redis instances
 	oldClient := redis.NewClient(&redis.Options{
 		Addr: oldRedisURL,
 		DB:   0,
@@ -41,102 +36,66 @@ func main() {
 		DB:   0,
 	})
 
-	// Get the number of keys in the old Redis database
 	oldKeysCount, err := oldClient.DBSize(context.Background()).Result()
 	if err != nil {
 		log.Fatalf("Error getting the count of keys from the old Redis database: %v", err)
 	}
 
-	// Flush all data from the new Redis database
 	_ = newClient.FlushAll(context.Background())
 
-	// Get all keys from the old Redis database
 	keys, err := oldClient.Keys(context.Background(), "*").Result()
 	if err != nil {
 		log.Fatalf("Error getting keys: %v", err)
 	}
 
-	// Initialize the progress bar with the total number of keys to transfer
 	bar := progressbar.Default(int64(len(keys)))
 
-	// Variables to measure the total time taken for GET and SET operations
-	totalGetTime := time.Duration(0)
-	totalSetTime := time.Duration(0)
-
-	// Create a Redis pipeline for bulk operations
-	oldPipeline := oldClient.Pipeline()
 	newPipeline := newClient.Pipeline()
 
-	// Channel to receive keys and communicate between goroutines
 	keysChan := make(chan string, len(keys))
 
-	// Create a WaitGroup to wait for all goroutines to finish
 	var wg sync.WaitGroup
 	numWorkers := 100
 	wg.Add(numWorkers)
 
-	// Start goroutines for GET and SET operations
 	for i := 0; i < numWorkers; i++ {
 		go func() {
 			defer wg.Done()
 			for key := range keysChan {
 				if key == "" {
-					return // Exit the goroutine when signaled
+					return
 				}
 
-				// Measure time taken for GET operation
-				startTime := time.Now()
 				dumpData, err := oldClient.Dump(context.Background(), key).Result()
 				if err != nil {
 					log.Fatalf("Error getting dump data for key %s: %v", key, err)
 				}
-				getTime := time.Since(startTime)
-				totalGetTime += getTime
 
-				// Add the SET operation to the pipeline
-				startTime = time.Now()
 				newPipeline.Restore(context.Background(), key, 0, dumpData)
-				setTime := time.Since(startTime)
-				totalSetTime += setTime
 
-				// Increment the progress bar to show the key transfer progress
 				bar.Add(1)
 			}
 		}()
 	}
 
-	// Add keys to the channel to be processed by goroutines
 	for _, key := range keys {
 		keysChan <- key
 	}
 
-	// Close the keysChan channel to signal that all keys have been added
 	close(keysChan)
-
-	// Wait for all goroutines to finish before executing the pipelines
 	wg.Wait()
-
-	// Execute the pipelines
-	_, err = oldPipeline.Exec(context.Background())
-	if err != nil {
-		log.Fatalf("Error executing the old Redis pipeline: %v", err)
-	}
 
 	_, err = newPipeline.Exec(context.Background())
 	if err != nil {
 		log.Fatalf("Error executing the new Redis pipeline: %v", err)
 	}
 
-	// Get the number of keys in the new Redis database
 	newKeysCount, err := newClient.DBSize(context.Background()).Result()
 	if err != nil {
 		log.Fatalf("Error getting the count of keys from the new Redis database: %v", err)
 	}
 
-	// Print the transfer summary
 	log.Println("Data transfer completed successfully.")
 	log.Printf("Number of keys in the old Redis database: %d", oldKeysCount)
 	log.Printf("Number of keys in the new Redis database: %d", newKeysCount)
-	log.Printf("Total time taken for GET operations: %v", totalGetTime)
-	log.Printf("Total time taken for SET operations: %v", totalSetTime)
 }
