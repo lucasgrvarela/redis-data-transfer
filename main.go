de75b84cee740edf397c0dcca19793643c8b8b41
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2023-07-25 12:24:33 -0300

    faster working version, set,get time wrong to be fixed

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -62,28 +63,68 @@ func main() {
 	totalGetTime := time.Duration(0)
 	totalSetTime := time.Duration(0)
 
-	// Iterate through each key and transfer data from old to new Redis database
+	// Create a Redis pipeline for bulk operations
+	oldPipeline := oldClient.Pipeline()
+	newPipeline := newClient.Pipeline()
+
+	// Channel to receive keys and communicate between goroutines
+	keysChan := make(chan string, len(keys))
+
+	// Create a WaitGroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
+	numWorkers := 100
+	wg.Add(numWorkers)
+
+	// Start goroutines for GET and SET operations
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+			for key := range keysChan {
+				if key == "" {
+					return // Exit the goroutine when signaled
+				}
+
+				// Measure time taken for GET operation
+				startTime := time.Now()
+				dumpData, err := oldClient.Dump(context.Background(), key).Result()
+				if err != nil {
+					log.Fatalf("Error getting dump data for key %s: %v", key, err)
+				}
+				getTime := time.Since(startTime)
+				totalGetTime += getTime
+
+				// Add the SET operation to the pipeline
+				startTime = time.Now()
+				newPipeline.Restore(context.Background(), key, 0, dumpData)
+				setTime := time.Since(startTime)
+				totalSetTime += setTime
+
+				// Increment the progress bar to show the key transfer progress
+				bar.Add(1)
+			}
+		}()
+	}
+
+	// Add keys to the channel to be processed by goroutines
 	for _, key := range keys {
-		// Measure time taken for GET operation
-		startTime := time.Now()
-		dumpData, err := oldClient.Dump(context.Background(), key).Result()
-		if err != nil {
-			log.Fatalf("Error getting dump data for key %s: %v", key, err)
-		}
-		getTime := time.Since(startTime)
-		totalGetTime += getTime
+		keysChan <- key
+	}
 
-		// Measure time taken for SET operation
-		startTime = time.Now()
-		err = newClient.Restore(context.Background(), key, 0, dumpData).Err()
-		if err != nil {
-			log.Fatalf("Error restoring data for key %s: %v", key, err)
-		}
-		setTime := time.Since(startTime)
-		totalSetTime += setTime
+	// Close the keysChan channel to signal that all keys have been added
+	close(keysChan)
 
-		// Increment the progress bar to show the key transfer progress
-		bar.Add(1)
+	// Wait for all goroutines to finish before executing the pipelines
+	wg.Wait()
+
+	// Execute the pipelines
+	_, err = oldPipeline.Exec(context.Background())
+	if err != nil {
+		log.Fatalf("Error executing the old Redis pipeline: %v", err)
+	}
+
+	_, err = newPipeline.Exec(context.Background())
+	if err != nil {
+		log.Fatalf("Error executing the new Redis pipeline: %v", err)
 	}
 
 	// Get the number of keys in the new Redis database
